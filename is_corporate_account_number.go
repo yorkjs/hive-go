c4acb4a0eb5d905f76c54d88298ea4f91081e3b3
musicode <musicode@qq.com>
2026-06-11 11:33:23 +0800

    调整对公账户的判断正则

--- is_corporate_account_number.go
+++ is_corporate_account_number.go
@@ -6,6 +6,6 @@ import "regexp"
 // value: 对公账户号码
 // 返回: 是否为对公账户
 func IsCorporateAccountNumber(value string) bool {
-	matched, _ := regexp.MatchString(`^\d{9,22}$`, value)
+	matched, _ := regexp.MatchString(`^\d{9,32}$`, value)
 	return matched
 }
