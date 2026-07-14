a2b1c1bdbd8943463abb7543fbba372bb4364bf2
musicode <musicode@qq.com>
2026-07-15 01:07:57 +0800

    新增星期常量

---
+++ constant_week.go
@@ -0,0 +1,22 @@
+package hive
+
+// 周日
+const WEEK_SUNDAY = 0
+
+// 周一
+const WEEK_MONDAY = 1
+
+// 周二
+const WEEK_TUESDAY = 2
+
+// 周三
+const WEEK_WEDNESDAY = 3
+
+// 周四
+const WEEK_THURSDAY = 4
+
+// 周五
+const WEEK_FRIDAY = 5
+
+// 周六
+const WEEK_SATURDAY = 6
